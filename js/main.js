bf5c0b56c5455db63f02ee80a6c955363596bbe3
jashwant <jashwantsingh@gmail.com>
2013-04-28 00:01:17 +0530

    turned autoplay off by default

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function ($) {
     cycle             :   1,
     shuffle           :   0,
     firstSlideIndex   :   1,
-    autoPlay          :   1, 
+    autoPlay          :   0, 
     pauseOnHover      :   1,
     autoPlayDelay     :   5000,
     animateFirstSlide :   1,
